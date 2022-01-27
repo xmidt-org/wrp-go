5acbd902ae2b8b6bc1e31cea75b8d682cd4371e3
mtrinh11 <mtrinh11@gmail.com>
2022-01-27 11:07:18 -0800

    initial client work (#65)
    * initial client
    
    * changed parameters
    
    * incorporate response parameter
    
    * changed copyright, error on response, and used decoder instead of unmarshalling
    
    * added testing file but no tests
    
    * linter
    
    * added unit tests
    
    * added unit test coverage
    
    * added unit test coverage
    
    * unit test mock_test
    
    * cleaned mockHTTPClient for unit tests
    
    * fixed merge conflicts
    
    * unit tests
    
    * removed encode failure unit test
    
    * changed client check to New and added URL check
    
    * changed New() parameter
    
    * changed client fields to be unexported
    
    * Update wrpclient/wrpclient.go
    
    Co-authored-by: Kristina Pathak <kmspring57@gmail.com>
    
    * fixed parameter names
    
    Co-authored-by: Kristina Pathak <kmspring57@gmail.com>

--- go.mod
+++ go.mod
@@ -3,14 +3,13 @@ module github.com/xmidt-org/wrp-go/v3
 go 1.13
 
 require (
-	github.com/davecgh/go-spew v1.1.1-0.20171005155431-ecdeabc65495
+	github.com/davecgh/go-spew v1.1.1
 	github.com/go-kit/kit v0.8.0
 	github.com/go-logfmt/logfmt v0.3.0 // indirect
 	github.com/go-stack/stack v1.8.0 // indirect
 	github.com/kr/logfmt v0.0.0-20140226030751-b84e30acd515 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	github.com/stretchr/objx v0.1.2-0.20180825064932-ef50b0de2877 // indirect
-	github.com/stretchr/testify v1.2.1
+	github.com/stretchr/testify v1.7.0
 	github.com/ugorji/go/codec v1.2.6
+	github.com/xmidt-org/httpaux v0.3.0
 	github.com/xmidt-org/webpa-common v1.3.2
 )
--- wrpclient/mocks_test.go
+++ wrpclient/mocks_test.go
@@ -15,29 +15,24 @@
  *
  */
 
-package wrpendpoint
+package wrpclient
 
 import (
-	"context"
+	"bytes"
+	"io/ioutil"
+	"net/http"
 
 	"github.com/stretchr/testify/mock"
 )
 
-type mockService struct {
+type mockHTTPClient struct {
 	mock.Mock
 }
 
-func (m *mockService) ServeWRP(ctx context.Context, r Request) (Response, error) {
-	arguments := m.Called(ctx, r)
-	first, _ := arguments.Get(0).(Response)
-	return first, arguments.Error(1)
-}
-
-type mockReader struct {
-	mock.Mock
-}
-
-func (m *mockReader) Read(p []byte) (int, error) {
-	arguments := m.Called(p)
-	return arguments.Int(0), arguments.Error(1)
+func (m *mockHTTPClient) Do(req *http.Request) (*http.Response, error) {
+	args := m.Called(req)
+	return &http.Response{
+		StatusCode: args.Int(0),
+		Body:       ioutil.NopCloser(bytes.NewBuffer(args.Get(1).([]byte))),
+	}, nil
 }
